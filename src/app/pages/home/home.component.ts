561bd9660cbacaa13ed4a8aa7951fdb873703e33
Yurii Khomych <yuriikhomych94@gmail.com>
2020-08-23 15:02:18 +0300

    Add glow

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/shared/interfaces/product.interface';
+import { OrdersService } from 'src/app/shared/services/orders.service';
+import { ProductsService } from 'src/app/shared/services/products.service';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  pizzaProducts: Array<IProduct> = [];
+  localProducts: Array<IProduct> = [];
+
+  constructor(private _ordersService: OrdersService,
+    private _productService: ProductsService) { }
 
   ngOnInit(): void {
+    this._getProducts();
+  }
+
+  private _getProducts(): void {
+    this._productService.getProduct().subscribe(data => {
+      this.pizzaProducts = data.filter(product => product.category.nameEN === 'pizza');
+    });
+  }
+
+  addToBasket(product: IProduct): void {
+    if (localStorage.length > 0 && localStorage.getItem('basket')) {
+      this.localProducts = JSON.parse(localStorage.getItem('basket'));
+      if (this.localProducts.some(prod => prod.id === product.id)) {
+        const index = this.localProducts.findIndex(prod => prod.id === product.id);
+        this.localProducts[index].count += product.count;
+      } else {
+        this.localProducts.push(product);
+      }
+      localStorage.setItem('basket', JSON.stringify(this.localProducts));
+    } else {
+      this.localProducts.push(product);
+      localStorage.setItem('basket', JSON.stringify(this.localProducts));
+    }
+    product.count = 1;
+    this._ordersService.basket.next(product);
+  }
+
+
+  productCount(product: IProduct, status: boolean): void {
+    if (status) {
+      product.count++;
+    } else {
+      if (product.count > 1) {
+        product.count--;
+      }
+    }
   }
 
 }
+
